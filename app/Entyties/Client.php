75b5f884877293de4b4a79f85a8ea8abc9b0c896
Jefferson Grassine <grassini@ig.com.br>
2015-11-03 12:23:16 -0200

    Fase_1.4

--- app/Entyties/Client.php
+++ app/Entyties/Client.php
@@ -23,7 +23,7 @@ class Client extends Model implements Transformable
     public function user()
     {
         #relancionando de 1 para 1
-        return $this->hasOne(User::class);
+        return $this->hasOne(User::class, 'id', 'user_id');
     }
 
 }
